ed141f331855197bdc04c1efd4522c80d7d39c79
Karun Narayan <narayankarun@gmail.com>
2019-05-17 22:13:53 -0700

    add seperate store and translate button testing it out

--- public/makeRequest.js
+++ public/makeRequest.js
@@ -1,6 +1,7 @@
 "strict mode";
 
-let myTranslations;
+let myEnglish;
+let myTranslation;
 
 // Do a CORS request to get Davis weather hourly forecast
 
@@ -14,7 +15,8 @@ function createCORSRequest(method, url) {
 // Make the actual CORS request.
 function makeCorsRequest() {
    let input = document.getElementById("myWord").value;
-   let url = "translate?english="+input;   
+   let url = "translate?english="+input;
+   myEnglish = input;   
    let xhr = createCORSRequest('GET', url);
 
   // checking if browser does CORS
@@ -29,7 +31,7 @@ function makeCorsRequest() {
       let object = JSON.parse(responseStr);  // turn it into an object
       console.log(JSON.stringify(object, undefined, 2));  // print it out as a string, nicely formatted
       let myWord = JSON.stringify(object.Spanish, undefined, 2);
-      myTranslations = myWord;
+      myTranslation = myWord;
       console.log(myWord);
       document.getElementById("outputGoesHere").innerHTML = myWord;
     };
@@ -42,33 +44,49 @@ function makeCorsRequest() {
   xhr.send();
 }
 
+function insertCallback(err) {
+  if (err) { 
+      console.log(err); 
+  }
+} 
+
 function makeCorsRequestStore() {
-  /*let input = document.getElementById("myWord").value;
-  let url = "translate?english="+input;   
-  let xhr = createCORSRequest('GET', url);
+    const sqlite3 = require("sqlite3").verbose();  // use sqlite
+    const fs = require("fs"); // file system
+    const dbFileName = "Flashcards.db";
+    // makes the object that represents the database in our code
+    const db = new sqlite3.Database(dbFileName);  // object, not database.
 
- // checking if browser does CORS
- if (!xhr) {
-   alert('CORS not supported');
-   return;
- }
+    //let input = document.getElementById("myWord").value;
+    let url = "store?english="+myEnglish+"&korean="+myTranslation;   
+    let xhr = createCORSRequest('GET', url);
 
- // Load some functions into response handlers.
- xhr.onload = function() {
-     let responseStr = xhr.responseText;  // get the JSON string 
-     let object = JSON.parse(responseStr);  // turn it into an object
-     console.log(JSON.stringify(object, undefined, 2));  // print it out as a string, nicely formatted
-     let myWord = JSON.stringify(object.Spanish, undefined, 2);
-     console.log(myWord);
-     document.getElementById("outputGoesHere").innerHTML = myWord;
-   };
+    // checking if browser does CORS
+    if (!xhr) {
+      alert('CORS not supported');
+      return;
+    }
 
- xhr.onerror = function() {
-   alert('Woops, there was an error making the request.');
- };
+    // Load some functions into response handlers.
+    xhr.onload = function() {
+      if ((myEnglish != undefined) && (myTranslation != undefined)) {
+        let eng = myEnglish;
+        let kor = myTranslation;
+        const cmdStr = 'INSERT into Flashcards (user, english,korean, seen, correct) VALUES (1, @0, @1, 0, 0)';
+        db.run(cmdStr, eng, kor, insertCallback);
+        console.log("We're in boyz");
+        dumpDB(); 
+        res.json("We put it in the database ");
+      }
+        
+    };
 
- // Actually send request to server
- xhr.send();*/
+    xhr.onerror = function() {
+      alert('Woops, there was an error making the request.');
+    };
+
+    // Actually send request to server
+    xhr.send();
 }
 
 // run this code to make request when this script file gets executed 
