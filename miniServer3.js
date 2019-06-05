3560565edc6b253fed6f4c71ca86c313529c025c
Karun Narayan <narayankarun@gmail.com>
2019-06-04 21:39:07 -0700

    changed if and else back

--- miniServer3.js
+++ miniServer3.js
@@ -166,10 +166,10 @@ function gotProfile(accessToken, refreshToken, profile, done) {
     const dbFileName = "Users.db";
     // makes the object that represents the database in our code
     const db = new sqlite3.Database(dbFileName);  // object, not database.
-    const checkUser = 'INSERT into Users (googleID,firstName,lastName) VALUES (@0, @1, @2)';
+    const checkUser = 'SELECT * FROM Users WHERE googleID = '+ profile.id;
     db.run(checkUser, function userCheckCallback(err){
         if(err){
-            console.log("Entry exists");
+             console.log("Entry exists");
             console.log("Checked correctly");
             done(null, dbRowID); 
             console.log("daniels");
@@ -178,8 +178,8 @@ function gotProfile(accessToken, refreshToken, profile, done) {
         } 
         else {
             console.log("IM HEREEEEEEEEEEEEEEEEEE");
-            // const cmdStr = 
-            // db.run(cmdStr, dbRowID, userFirstName, userLastName, insertCallback);
+            const cmdStr = 'INSERT into Users (googleID,firstName,lastName) VALUES (@0, @1, @2)';
+            db.run(cmdStr, dbRowID, userFirstName, userLastName, insertCallback);
             done(null, dbRowID); 
         }   
 
